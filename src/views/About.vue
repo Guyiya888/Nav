5b680401669e5661924f6c219e44edc0a940f38b
Guyiya888 <2015144041@qq.com>
2025-10-21 23:14:46 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,9 @@
-<title>关于 GUYI NAV</title>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>关于 GUYI NAV</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
